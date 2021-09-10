e57117ec83820e1be2c217ec70d071671683c3c7
lim ho <dlagh123@gmail.com>
2021-09-10 21:07:50 +0900

    .

--- components/ProductList.vue
+++ components/ProductList.vue
@@ -5,21 +5,20 @@
 </template>
 
 <script>
-import axios from 'axios'
+// import axios from 'axios'
 
 export default {
   // 상위 컴포넌트 즉 pages 에서만 사용가능
-  async asyncData() {
-    const response = await axios.get('http://localhost:3000/products')
-    const products = response.data
-    return { products }
-  },
-  data() {
-    return {
-      products: [],
-    }
-  },
-
+  // async asyncData() {
+  //   const response = await axios.get('http://localhost:3000/products')
+  //   const products = response.data
+  //   return { products }
+  // },
+  // data() {
+  //   return {
+  //     products: [],
+  //   }
+  // },
   // async created(){
   //   const response = await axios.get('http://localhost:3000/products');
   //   this.products = response.data
