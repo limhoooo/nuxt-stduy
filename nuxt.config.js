77730055b700633a818c7693f0f31a023b413372
lim ho <dlagh123@gmail.com>
2021-09-14 16:08:16 +0900

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: '임호 nuxt 테스트 crud 페이지 ㅋ' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
